3c06659fe6faeb2aceb4fdef67593ebca5a555cc
Amrit Deep Dhungana <amritdeepdhungana@hotmail.com>
2013-11-08 14:45:27 +0545

    add animation for cloud and car for its movement

---
+++ js/move_car.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+	// alert("HO");
+	
+	// $('#cloud1').hide();
+	// $('#cloud2').hide();
+
+	// $('#car').hide();
+
+
+
+	// $(':button').click(function(){
+	// 	$('#cloud1').show();
+	// 	$('#cloud2').show();
+	// 	$('#car').show();
+	// });
+
+
+});
