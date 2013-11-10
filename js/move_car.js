662803c354846ba325c89906506dc6d37f9806f2
Amrit Deep Dhungana <amritdeepdhungana@hotmail.com>
2013-11-10 14:51:22 +0545

    Done with moving car animation, cloud animation and tree animation

--- js/move_car.js
+++ js/move_car.js
@@ -1,11 +1,38 @@
 $(document).ready(function(){
-	$('#cloud1').removeClass("move_cloud1");
-	$('#cloud2').removeClass("move_cloud2");
-	$('#car').removeClass("move_car");
+	var fifteen = $(':button#fifteen');
+	var thirty = $(':button#thirty');
+	var forty_five = $(':button#forty-five');
+	var sixty = $(':button#sixty');
 
-	$(':button').click(function(){
-		$('#cloud1').addClass("move_cloud1");
-		$('#cloud2').addClass("move_cloud2");
-		$('#car').addClass("move_car");
+	// $('#cloud1').removeClass("move_cloud1");
+	// $('#cloud2').removeClass("move_cloud2");
+	$('#big_tree').removeClass("move_big_tree");
+	$('#small_tree').removeClass("move_small_tree");
+	$('#car').removeClass("move_car").hide();
+
+	// drive(fifteen, 15s);
+
+
+	// fifteen.click(function(){
+	// 	$(this).css('animation-duration': '15s');
+	// });
+
+	$(':button').bind('click', function(){
+		// alert("hello");
+		// $('#cloud1').addClass("move_cloud1");
+		// $('#cloud2').addClass("move_cloud2");
+		$('#big_tree').addClass("move_big_tree");
+		$('#small_tree').addClass("move_small_tree");
+		$('#car').addClass("move_car").show();
 	});
 });
+
+// function drive(variable, time_duration){
+// 	variable.click(function(){
+// 		// alert($(this).now());
+// 		// alert(time_duration);
+// 		$('#cloud1').addClass("move_cloud1").css('animation-duration': 'time_duration');
+// 		$('#cloud2').addClass("move_cloud2").css('animation-duration': 'time_duration');
+// 		$('#car').addClass("move_car").css('animation-duration': 'time_duration');
+// 	});
+// }
